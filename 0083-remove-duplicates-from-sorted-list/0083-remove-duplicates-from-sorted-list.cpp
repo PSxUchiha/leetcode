eb23d087dd643957af792db781b5a680f61935a7
Priyanshu Sharma <72612676+PSxUchiha@users.noreply.github.com>
2025-08-13 01:45:52 +0530

    0083-REMOVE-DUPLICATES-FROM-SORTED-LIST Stats: Time: 0 ms (100%), Space: 16.3 MB (36.95%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(0, head);  
+        ListNode* prev = &dummy;  
+
+        while (head) {
+            if (head->next && head->val == head->next->val) {
+                int n = head->val;
+                while (head->next && head->next->val == n) {
+                    head = head->next;
+                }
+                prev->next = head; 
+            } else {
+                prev = head;       
+                head = head->next; 
+            }
+        }
+        return dummy.next;
+    }
+};
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ leetcode stuff
 |  |
 | ------- |
 | [0082-remove-duplicates-from-sorted-list-ii](https://github.com/PSxUchiha/leetcode/tree/master/0082-remove-duplicates-from-sorted-list-ii) |
+| [0083-remove-duplicates-from-sorted-list](https://github.com/PSxUchiha/leetcode/tree/master/0083-remove-duplicates-from-sorted-list) |
 ## Two Pointers
 |  |
 | ------- |
