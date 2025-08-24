5b4f234613c880c3180e63ac9f35eaf52f7c42f9
Priyanshu Sharma <72612676+ego1s1@users.noreply.github.com>
2025-08-24 18:05:52 +0530

    0136-SINGLE-NUMBER Stats: Time: 0 ms (100%), Space: 20.6 MB (83.22%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,7 +4,7 @@ public:
         int index = 0;
         for (int i = 0; i < nums.size(); i++) {
             index = index ^ nums[i]; //all numbers that appear twice
-        }                       //cancel each other out through XOR
+        }                           //cancel each other out through XOR
         return index;
     }
 };
